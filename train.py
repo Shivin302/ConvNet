fcc5f0e67b45ef7a179ce322afb7156747598e56
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-04-25 08:07:12 +0000

    part b 1

--- train.py
+++ train.py
@@ -46,13 +46,13 @@ device = torch.device("cuda:0" if use_cuda else "cpu")
 #cudnn.benchmark = True
 
 # Parameters for data loader
-params = {'batch_size':128,  # TODO: fill in the batch size. often, these are things like 32,64,128,or 256
+params = {'batch_size': 128,  # TODO: fill in the batch size. often, these are things like 32,64,128,or 256
           'shuffle': True,
-          'num_workers': 0
+          'num_workers': 2
           }
 # TODO: Hyper-parameters
-num_epochs = 15
-learning_rate = 1e-1
+num_epochs = 40
+learning_rate = 7e-4
 # NOTE: depending on your optimizer, you may want to tune other hyperparameters as well
 
 # Datasets
@@ -60,17 +60,17 @@ learning_rate = 1e-1
 if is_key_frame:
     # label_file_train =  '../data/hw6_mds189/keyframe_data_train.txt'
     # label_file_val  =  '../data/hw6_mds189/keyframe_data_val.txt'
-    label_file_train =  'C:/Users/Shivin/Desktop/CS 189/hw6/data/keyframe_data_train.txt'
-    label_file_val  =  'C:/Users/Shivin/Desktop/CS 189/hw6/data/keyframe_data_val.txt'
+    label_file_train =  '~/cs189/dataloader_files/keyframe_data_train.txt'
+    label_file_val  = '~/cs189/dataloader_files/keyframe_data_val.txt'
     # NOTE: the kaggle competition test data is only for the video frames, not the key frames
     # this is why we don't have an equivalent label_file_test with keyframes
 else:
     # label_file_train = '../data/hw6_mds189/videoframe_data_train.txt'
     # label_file_val = '../data/hw6_mds189/videoframe_data_val.txt'
     # label_file_test = '../data/hw6_mds189/videoframe_data_test.txt'
-    label_file_train = 'C:/Users/Shivin/Desktop/CS 189/hw6/data/videoframe_data_train.txt'
-    label_file_val = 'C:/Users/Shivin/Desktop/CS 189/hw6/data/videoframe_data_val.txt'
-    label_file_test = 'C:/Users/Shivin/Desktop/CS 189/hw6/data/videoframe_data_test.txt'
+    label_file_train = '~/cs189/dataloader_files/videoframe_data_train.txt'
+    label_file_val = '~/cs189/dataloader_files/videoframe_data_val.txt'
+    label_file_test = '~/cs189/dataloader_files/videoframe_data_test.txt'
 
 # TODO: you should normalize based on the average image in the training set. This shows
 # an example of doing normalization
@@ -105,6 +105,8 @@ if not is_key_frame:
                                                    transforms.Normalize(mean, std)
                                                ]))
     test_loader = data.DataLoader(test_dataset, **params)
+else:
+    test_loader = val_loader
 
 # TODO: one way of defining your model architecture is to fill in a class like NeuralNet()
 # NOTE: you should not overwrite the models you try whose performance you're keeping track of.
@@ -114,7 +116,7 @@ if not is_key_frame:
 #       you want to do is have to recode the layer structure for a model (whose performance
 #       you're reporting) because you forgot to e.g., compute the confusion matrix on its results
 #       or visualize the error modes of your (best) model
-class NeuralNet(nn.Module):
+class NeuralNetTest(nn.Module):
     def __init__(self):
         super(NeuralNet, self).__init__()
         # you can define some common layers, for example:
@@ -160,6 +162,68 @@ class NeuralNet1(nn.Module):
         x = self.fc3(x)
         return x
 
+class NeuralNet(nn.Module):
+    def __init__(self):
+        super(NeuralNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 12, 5, stride = 2)
+        self.conv2 = nn.Conv2d(12, 96, 3)
+        self.conv3 = nn.Conv2d(96, 48, 3, 1)
+#        self.conv4 = nn.Conv2d(384, 256, 3, 1)
+#        self.conv5 = nn.Conv2d(256, 256, 3, 1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(48 * 26 * 12, 4096)
+        self.fc2 = nn.Linear(4096, 512)
+        self.fc3 = nn.Linear(512, 8)
+        self.dropout = nn.Dropout()
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x))) # x -> convolution -> ReLU -> max pooling
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+#        x = F.relu(self.conv4(x))
+#        x = self.pool(F.relu(self.conv5(x)))
+#        x = nn.AdaptiveAvgPool2d((6,6))(x)
+#        print(x.shape)
+        num_feat = np.prod(x.shape[1:])
+        x = x.view(-1, num_feat)
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = self.fc3(x)
+        return x
+
+class NeuralNet(nn.Module):
+    def __init__(self):
+        super(NeuralNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 48, 11, 4)
+        self.conv2 = nn.Conv2d(48, 128, 5)
+        self.conv3 = nn.Conv2d(128, 192, 3, 1)
+        self.conv4 = nn.Conv2d(192, 192, 3, 1)
+#        self.conv5 = nn.Conv2d(256, 256, 3, 1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(192 * 4 * 1, 400)
+        self.fc2 = nn.Linear(400, 200)
+        self.fc3 = nn.Linear(200, 8)
+        self.dropout = nn.Dropout()
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x))) # x -> convolution -> ReLU -> max pooling
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = self.pool(F.relu(self.conv4(x)))
+#        x = self.pool(F.relu(self.conv5(x)))
+#        x = nn.AdaptiveAvgPool2d((6,6))(x)
+#        print(x.shape)
+        num_feat = np.prod(x.shape[1:])
+        x = x.view(-1, num_feat)
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = self.fc3(x)
+        return x
+
 model = NeuralNet().to(device)
 
 # if we're only testing, we don't want to train for any epochs, and we want to load a model
@@ -170,7 +234,15 @@ if not is_train:
 # Loss and optimizer
 criterion =  nn.CrossEntropyLoss() #TODO: define your loss here. hint: should just require calling a built-in pytorch layer.
 # NOTE: you can use a different optimizer besides Adam, like RMSProp or SGD, if you'd like
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+import adabound
+#optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+optimizer = adabound.AdaBound(model.parameters(), lr=learning_rate, final_lr = 0.1, weight_decay = 1e-5)
+
+def accuracy(p, y):
+#    print(p.shape, y.shape)
+    p = p.cpu().argmax(dim=1).numpy()
+    y = y.cpu().numpy()
+    return np.sum(p == y)/len(p)
 
 # Train the model
 # Loop over epochs
@@ -192,22 +264,29 @@ for epoch in range(num_epochs):
         # NOTE: if you use Google Colab's tensorboard-like feature to visualize
         #       the loss, you do not need to plot it here. just take a screenshot
         #       of the loss curve and include it in your write-up.
-        loss_list.append(loss)
-
+        if i % 4 == 0:
+            loss_list.append(loss.data.cpu().numpy().max())
+        acc = accuracy(outputs, local_labels)
         # Backward and optimize
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
         if (i+1) % 4 == 0:
-            print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
-                   .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
-    for local_batch, local_labels in val_loader:
-        local_ims, local_labels = local_batch.to(device), local_labels.to(device)
-        outputs = model.forward(local_ims)
-        loss = criterion(outputs, local_labels)
-        val_loss_list.append(loss)
-
+            print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, TrainAcc: {}'
+                   .format(epoch+1, num_epochs, i+1, total_step, loss.item(), acc))
+    with torch.no_grad():
+        valacc = []
+        mean_val_loss = []
+        for local_batch, local_labels in val_loader:
+            local_ims, local_labels = local_batch.to(device), local_labels.to(device)
+            outputs = model.forward(local_ims)
+            loss = criterion(outputs, local_labels)
+            mean_val_loss.append(loss.data.cpu().numpy().max())
+            valacc.append(accuracy(outputs, local_labels))
+        print("Val Accuracy:", np.mean(valacc))
+#        print(mean_val_loss)
+        val_loss_list.append(np.mean(mean_val_loss))
 
 end = time.time()
 print('Time: {}'.format(end - start))
@@ -238,6 +317,9 @@ with torch.no_grad():
 pl = [p.cpu().numpy().tolist() for p in predicted_list]
 gt = [p.cpu().numpy().tolist() for p in groundtruth_list]
 
+np.save("preds_b", pl)
+np.save("true_labels_b", gt)
+
 # TODO: use pl and gt to produce your confusion matrices
 
 # view the per-movement accuracy
@@ -246,9 +328,29 @@ for id in range(len(label_map)):
     print('{}: {}'.format(label_map[id],sum([p and g for (p,g) in zip(np.array(pl)==np.array(gt),np.array(gt)==id)])/(sum(np.array(gt)==id)+0.)))
 
 # TODO: you'll need to run the forward pass on the kaggle competition images, and save those results to a csv file.
+
 if not is_key_frame:
     # your code goes here!
-    pass
+    def results_to_csv(y_test, name = "submission.csv"):
+        y_test = y_test.astype(int)
+        y_test = [label_map[i] for i in y_test]
+        df = pd.DataFrame({'Category': y_test})
+        def img_lab(label):
+            return str(x).zfill(4) + ".jpg"
+        df.index = df.index.map(label)
+        df.to_csv(name, index_label='Id')
 
+    with torch.no_grad():
+        preds = []
+        for (local_batch, local_labels) in test_loader:
+            # Transfer to GPU
+            local_ims, local_labels = local_batch.to(device), local_labels.to(device)
+            pred = model.forward(local_ims)
+            _, pred2 = torch.max(pred.data, 1)
+            preds.extend(list(pred2.cpu().numpy()))
+        results_to_csv(np.array(preds))
+
+np.save("train_loss_b", loss_list)
+np.save("val_loss_b", val_loss_list)
 # Save the model checkpoint
-torch.save(model.state_dict(), 'model.ckpt')
+torch.save(model.state_dict(), 'model_b.ckpt')
